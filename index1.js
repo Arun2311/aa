a9a072c531c1d20d72b7cea2880e0dbaf86cab0b
Arunachalam <cvarunachalam84@gmail.com>
2022-06-15 15:15:50 +0530

    yogesh add

---
+++ index1.js
@@ -0,0 +1,47 @@
+ let userList = [
+
+    {
+        name: "captain marvel",
+        age: 50,
+    },
+    {
+        name: "captain america",
+        age: 150,
+    },
+    {
+        name: "iron man",
+        age: 50
+    },
+    {
+        name: "spyderman",
+        age: 22
+    },
+    {
+        name: "Thor",
+        age: 1000
+    }
+
+ ]
+
+ ["arun","a","asa",]
+
+
+ let result = userList.find(a => a.name ==="iron man")
+
+ console.log(result);
+
+
+var newA = userList.map(function(v){
+
+
+    return v.age+1;
+
+
+});
+ 
+
+console.log(newA);
+
+
+
+
